1f7e6a13dcf53bc6b99459a866c1c6003f42f341
Vitaly <msk.vitaly@gmail.com>
2023-05-15 07:08:57 +0300

    [PROS-32] Виджет Yandex.Metrica #8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
       <meta name="description" content="news aggregator" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/icon.svg" />
-      <script defer type="text/javascript" src="/yandex-metrica.js" />
     </Head>
     <LayoutMain />
   </>
